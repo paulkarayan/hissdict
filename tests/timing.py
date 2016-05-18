dc10661ac3210ed14b94dbe721f69f7485bfeedd
Paul Karayan <paulkarayan@gmail.com>
2016-05-17 17:31:50 -0700

    performance tests with some todos

--- tests/timing.py
+++ tests/timing.py
@@ -17,7 +17,7 @@ def timing(func):
         start = time()
         result = func(*args, **kwargs)
         end = time()
-        # print 'func:%r args:[%r, %r] took: %2.4f sec' % \
-        #   (func.__name__, args, kwargs, end-start)
+        print 'func:%r args:[%r, %r] took: %2.4f sec' % \
+          (func.__name__, args, kwargs, end-start)
         return result, func.__name__, end-start
     return wrap
